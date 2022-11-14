8c4156d9643408b357e3773eb065c1756912fa3c
guilherme <grgomes82@gmail.com>
2022-11-14 15:11:30 -0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId  } from 'mongodb';
 import joi from 'joi';
 import dayjs from 'dayjs';
+import { stripHtml } from "string-strip-html";
+
 
 
 const app = express();
@@ -16,10 +18,10 @@ let db;
 
 
 try {
-    await mongoClient.connect();
-    db = mongoClient.db("batepapouol");
-  } catch (err) {
-    console.log(err);
+  await mongoClient.connect();
+  db = mongoClient.db("batepapouol");
+} catch (err) {
+  console.log(err);
 }
 
 const batepapoSchema = joi.object({
@@ -31,48 +33,49 @@ const batepapomsgSchema = joi.object({
   type: joi.string().valid('message', 'private_message').required(),
 })
 
+
 app.post('/participants', async (req, res) => {
-  const {name} = req.body;
+  const { name } = req.body;
 
-  const validation = batepapoSchema.validate(req.body, {abortEarly : false})
+  const validation = batepapoSchema.validate(req.body, { abortEarly: false })
 
-  if(validation.error){
+  if (validation.error) {
     const errors = validation.error.details.map((detail) => detail.message);
     res.status(422).send(errors)
-    return 
+    return
   }
-  try{
-    const isUser = await db.collection('participants').findOne({name: name})
-    
-    if(isUser){
+  try {
+    const isUser = await db.collection('participants').findOne({ name: name })
+
+    if (isUser) {
       return res.sendStatus(409);
     }
     await db.collection('participants').insertOne({
-      name,
+      name:stripHtml(name).result.trim(),
       lastStatus: Date.now()
     })
     await db.collection('participants').insertOne({
       from: name,
-      to: 'Todos', 
-      text: 'entra na sala...', 
-      type: 'status', 
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
       time: dayjs().format("HH:mm:ss")
     });
 
     res.sendStatus(201);
 
-  } catch(err) {
+  } catch (err) {
     res.sendStatus(err);
   }
 
 
 })
 
-app.get('/participants', async (req,res) => {
-  try{
+app.get('/participants', async (req, res) => {
+  try {
     let participants = await db.collection('participants').find().toArray();
     res.send(participants);
-  } catch(error) {
+  } catch (error) {
     res.sendStatus(500)
   }
 })
@@ -80,90 +83,164 @@ app.get('/participants', async (req,res) => {
 
 
 
-app.post('/messages', async (req,res) => {
+app.post('/messages', async (req, res) => {
   const from = req.headers.user;
-  const {to, text, type} = req.body;
+  const { to, text, type } = req.body;
 
-  if(!from){
+  if (!from) {
     res.sendStatus(401);
     return;
   }
-  const validateParticipant = batepapomsgSchema.validate(req.body, {abortEarly : false})
-  if(validateParticipant.error){
+  const validateParticipant = batepapomsgSchema.validate(req.body, { abortEarly: false })
+  if (validateParticipant.error) {
     const errors = validateParticipant.error.details.map((detail) => detail.message);
     res.status(422).send(errors)
-    return 
+    return
   }
 
-  try{
+  try {
     await db.collection('message').insertOne({
-      from,
-      to,
-      text,
-      type
+      from: stripHtml(from).result.trim(),
+      to: stripHtml(to).result.trim(),
+      text: stripHtml(text).result.trim(),
+      type: stripHtml(type).result.trim()
     });
     res.sendStatus(201);
-  } catch(err) {
+  } catch (err) {
     res.sendStatus(err);
   }
 
 })
 
-app.get('/messages', async (req,res) => {
+app.get('/messages', async (req, res) => {
   const limit = parseInt(req.query.limit);
   const from = req.headers.user;
 
-  try{
+  try {
     let messages = await db.collection('message').find().toArray();
     let msgfiltered = (messages.filter((message) => message.from === from))
     res.send(msgfiltered.slice(msgfiltered.length - limit, msgfiltered.length));
-  } catch(error) {
+  } catch (error) {
     res.sendStatus(500);
   }
 })
 
-app.post('/status', async (req,res) => {
+app.post('/status', async (req, res) => {
   const usuario = req.headers.user;
   console.log(usuario);
-  
 
-  try{
-   
-    let oneparticipant = await db.collection('participants').findOne({name:usuario})
 
-    if(!oneparticipant){
+  try {
+
+    let oneparticipant = await db.collection('participants').findOne({ name: usuario })
+
+    if (!oneparticipant) {
       res.status(404).send("Usuário não encontrado!");
       return;
     }
-    
-    await db.collection('participants').updateOne({name: usuario}, {$set: {lastStatus:Date.now()}});
+
+    await db.collection('participants').updateOne({ name: usuario }, { $set: { lastStatus: Date.now() } });
     res.sendStatus(200);
-    } catch(error) {
+  } catch (error) {
     res.send(error);
   }
- 
+
   // db.student.updateOne({name: "Annu"}, {$set:{age:25}})
 
 
 
 })
+
+
+// const sim =  await db.collection("message").find({_id: ObjectId(id)});
+    
+app.delete('/messages/:id', async (req, res) => {
+  const {id} = req.params;
+  const usuario = req.headers.user;
+
+  try {
+    const findId = await db.collection("message").findOne({_id: ObjectId(id)});
+    if(!findId){
+      res.status(404).send("Id não encontrado");
+      return;
+    }
+    if(findId.from !== usuario ){
+      res.status(401).send("Usuário não encontrado!");
+      return;
+    }
+    await db.collection("message").deleteOne({_id: ObjectId(id)});
+    res.status(200).send("Documento apagado com sucesso!");
+  } catch (err) {
+    console.log(err);
+    res.status(404).send('Mensagem não encontrada')
+}});
+
+app.put('/messages/:id', async (req, res) => {
+  const {id} = req.params;
+  const usuario = req.headers.user;
+
+  try {
+    const findId = await db.collection("message").findOne({_id: ObjectId(id)});
+    if(!findId){
+      res.status(404).send("Id não encontrado");
+      return;
+    }
+    if(findId.from !== usuario ){
+      res.status(401).send("Usuário não encontrado!");
+      return;
+    }
+    const validateParticipant = batepapomsgSchema.validate(req.body, { abortEarly: false })
+    if (validateParticipant.error) {
+      const errors = validateParticipant.error.details.map((detail) => detail.message);
+      res.status(422).send(errors)
+      return
+    }
+    await db.collection('message').updateOne({ 
+			_id: findId._id 
+		}, { $set: req.body })
+				
+		res.sendStatus(200);
+  } catch (err) {
+    res.status(404).send('Mensagem não encontrada');
+    return;
+}});
+
+
+
+// db.restaurant.updateOne(
+//   { "name" : "Central Perk Cafe" },
+//   { $set: { "violations" : 3 } }
+// );
+
 // let filterDelete = await db.collection('participants').find({name: {$exists:true}}).toArray();
 // let filter1 = filterDelete.filter((last) => Date.now() - last.lastStatus < 3600000)
 // console.log(filter1)
-setInterval(function(){
-  db.collection('participants').deleteMany({lastStatus: {"$lt":Date.now() - 3600000}})
-},25000)
-let filter = await db.collection('participants').find().toArray();
-console.log(filter)
+
+
+// setInterval(async function () {
+//   try {
+//     // let allParticipants = await db.collection('participants').find().toArray();
+//     db.collection('participants').deleteMany({ text: 'entra na sala...' });
+//     db.collection('participants').deleteMany({ lastStatus: { "$lt": Date.now() - 10000 } });
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }, 15000)
+
+// let filter = await db.collection('participants').find().toArray();
+// console.log(filter)
+
 
 // let filterDelete = db.collection('participants').find({ name: {$exists: true} });
 
 // let filter1 = filterDelete.filter((filt,index) => filt.name === "Vasco")
 // console.log(filter1[0].name.length)
-app.listen(5000, ()=> console.log("Rodando na porta 5000"));
+app.listen(5000, () => console.log("Rodando na porta 5000"));
 // db.products.find({"things.bottle":{"$exists":true}})
 
 // db.orders.deleteMany( { "stock" : "Brent Crude Futures", "limit" : { $gt : 48.88 } } );
 // db.inventory.find( { quantity: { $lt: 20 } } )
 
 // db.myCollection.deleteMany({modifiedOn : {"$lt" : new Date(2021, 12, 20}})
+// {from: 'xxx', to: 'Todos', text: 'sai da sala...', type: 'status', time: 'HH:MM:SS'}
+
